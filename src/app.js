c7c373dcb1bfd9ba0c74c14ae8acf57f1e07b8c4
Guilherme Neves <guineves1997@gmail.com>
2020-04-12 18:57:00 -0300

    Adicionado os arquivos de iniciação do express

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(helmet());
+
+module.exports = app;
