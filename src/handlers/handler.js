fc76c465e800a977d770bd954dd0b58da93c3e0d
Guilherme Neves <guineves1997@gmail.com>
2020-04-13 15:55:15 -0300

    Adicionada a rota de adicionar uma lista de cards

--- src/handlers/handler.js
+++ src/handlers/handler.js
@@ -7,9 +7,25 @@ exports.getAllCards = (req,res,next) => {
     });
 }
 
+// exports.getCardById = (req,res,next) => {
+//     repository.getCardByID((err, cards) => {
+//         if(err) return next(err);
+//         res.json(cards);
+//     });
+// }
+
 exports.insertANewCard = (req,res,next) => {
     let card = req.body;
     repository.insertANewCard(card, (err, resp) => {
+        if(err) return next(err);
+        //res.status(201).set('Location', )
+        res.json(resp);
+    });
+}
+
+exports.insertAListOfCards = (req,res,next) => {
+    let cards = req.body;
+    repository.insertAListOfCards(cards, (err, resp) => {
         if(err) return next(err);
         //res.status(201).set('Location', )
         res.json(resp);
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const handler = require('../handlers/handler'); 
 
 router.post('/card', handler.insertANewCard);
+
+router.post('/cards', handler.insertAListOfCards);
+
 router.get('/card', handler.getAllCards);
 
 module.exports = router;
